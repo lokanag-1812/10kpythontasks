ce3ec9af215768812579a7f402c04827227ffd61
loknag-1812 <lokanagagangadharadapa@gmail.com>
2026-07-07 23:38:26 +0530

    added problem solving

---
+++ problemsolving.py
@@ -0,0 +1,44 @@
+# TASK:20/05/2026
+# All Expressions / Formulas in Python
+
+a = 10
+b = 5
+c = 2
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+print("Floor Division =", a // b)
+print("Modulus =", a % b)
+print("Power =", a ** c)
+
+print("Average =", (a + b + c) / 3)
+print("Square =", a ** 2)
+print("Cube =", a ** 3)
+
+print("Area of Rectangle =", a * b)
+print("Perimeter of Rectangle =", 2 * (a + b))
+
+print("Area of Square =", c ** 2)
+print("Perimeter of Square =", 4 * c)
+
+print("Simple Interest =", (1000 * 5 * 2) / 100)
+print("Percentage =", (450 / 500) * 100)
+
+print("a > b =", a > b)
+print("a < b =", a < b)
+print("a == b =", a == b)
+print("a != b =", a != b)
+print("a >= b =", a >= b)
+print("a <= b =", a <= b)
+
+print("Logical AND =", (a > b) and (b > c))
+print("Logical OR =", (a < b) or (b > c))
+print("Logical NOT =", not(a < b))
+
+print("Bitwise AND =", a & b)
+print("Bitwise OR =", a | b)
+print("Bitwise XOR =", a ^ b)
+print("Left Shift =", a << 1)
+print("Right Shift =", a >> 1)
