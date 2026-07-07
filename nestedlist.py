8d66be3c5a864e6f9aea2843f81da33f1082e08d
loknag-1812 <lokanagagangadharadapa@gmail.com>
2026-07-08 00:34:16 +0530

    nestedlist

---
+++ nestedlist.py
@@ -0,0 +1,12 @@
+# TASK: Binary to Decimal Conversion
+
+binary = input("Enter a Binary Number: ")
+
+decimal = 0
+power = 0
+
+for digit in binary[::-1]:
+    decimal = decimal + int(digit) * (2 ** power)
+    power = power + 1
+
+print("Decimal =", decimal)
